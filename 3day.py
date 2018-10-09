57532704f82e37e8aec77473eb9f28685860c836
chandrahyd <43515767+chandrahyd@users.noreply.github.com>
2018-10-09 16:17:53 +0530

    3 days

---
+++ 3day.py
@@ -0,0 +1,7 @@
+import datetime
+today=datetime.datetime.today()
+yesterday=today-datetime.timedelta(days=1)
+tomorrow=today+datetime.timedelta(days=1)
+print("today:",today)
+print("yesterday:",yesterday)
+print("tomorrow:",tomorrow)
